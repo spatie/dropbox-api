8513d5dfb0fca1064cd5111d034a072230b46898
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-04-19 10:17:35 +0200

    code cleanup

--- src/Client.php
+++ src/Client.php
@@ -7,6 +7,15 @@ use GuzzleHttp\Psr7\StreamWrapper;
 
 class Client
 {
+    const THUMBNAIL_FORMAT_JPEG = 'jpeg';
+    const THUMBNAIL_FORMAT_PNG = 'png';
+
+    const THUMBNAIL_SIZE_XS = 'w32h32';
+    const THUMBNAIL_SIZE_S = 'w64h64';
+    const THUMBNAIL_SIZE_M = 'w128h128';
+    const THUMBNAIL_SIZE_L = 'w640h480';
+    const THUMBNAIL_SIZE_XL = 'w1024h768';
+
     protected $accessToken;
 
     protected $client;
@@ -16,92 +25,68 @@ class Client
         $this->accessToken = $accessToken;
 
         $this->client = new GuzzleClient([
-            'base_uri' => 'https://api.dropboxapi.com/2/',
             'headers' => [
                 'Authorization' => "Bearer {$this->accessToken}",
             ],
         ]);
     }
 
-    public function move(string $path, string $newPath): array
-    {
-        $path = $this->normalizePath($path);
-        $newPath = $this->normalizePath($newPath);
-
-        $response = $this->client->post('files/move', [
-            'json' => [
-                'from_path' => $path,
-                'to_path' => $newPath,
-            ]
-        ]);
-
-        return json_decode($response->getBody(), true);
-    }
-
+    /**
+     * Copy a file or folder to a different location in the user's Dropbox.
+     * If the source path is a folder all its contents will be copied.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-copy
+     */
     public function copy(string $path, string $newPath): array
     {
-        $path = $this->normalizePath($path);
-        $newPath = $this->normalizePath($newPath);
+        $parameters = [
+            'from_path' => $this->normalizePath($path),
+            'to_path' => $this->normalizePath($newPath),
+        ];
 
-        $response = $this->client->post('files/copy', [
-            'json' => [
-                'from_path' => $path,
-                'to_path' => $newPath,
-            ]
-        ]);
-
-        return json_decode($response->getBody(), true);
-    }
-
-    public function delete(string $path): array
-    {
-        $path = $this->normalizePath($path);
-
-        $response = $this->client->post('files/delete', [
-            'json' => [
-                'path' => $path,
-            ]
-        ]);
-
-        return json_decode($response->getBody(), true);
+        return $this->requestRPC('files/copy', $parameters);
     }
 
     /**
-     * @param string $path
+     * Create a folder at a given path.Create a folder at a given path.
      *
-     * @return mixed
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-create_folder
      */
-    public function createFolder(string $path)
+    public function createFolder(string $path): array
     {
-        $path = $this->normalizePath($path);
+        $parameters = [
+            'path' => $this->normalizePath($path),
+        ];
 
-        $response = $this->client->post('files/create_folder', [
-            'json' => [
-                'path' => $path,
-            ]
-        ]);
+        $object = $this->requestRPC('files/create_folder', $parameters);
 
-        $result = json_decode($response->getBody(), true);
+        $object['.tag'] = 'folder';
 
-        $result['.tag'] = 'folder';
-
-        return $result;
+        return $object;
     }
 
-    public function getMetadata(string $path)
+    /**
+     * Delete the file or folder at a given path.
+     * If the path is a folder, all its contents will be deleted too.
+     * A successful response indicates that the file or folder was deleted.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-delete
+     */
+    public function delete(string $path): array
     {
-        $path = $this->normalizePath($path);
+        $parameters = [
+            'path' => $this->normalizePath($path),
+        ];
 
-        $response = $this->client->post('files/get_metadata', [
-            'json' => [
-                'path' => $path,
-            ]
-        ]);
-
-        return json_decode($response->getBody(), true);
+        return $this->requestRPC('files/delete', $parameters);
     }
 
-    public function getFile(string $path)
+    /**
+     * Download a file from a user's Dropbox.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-download
+     */
+    public function download(string $path): resource
     {
         $dropboxApiArguments = [
             'path' => $this->normalizePath($path),
@@ -116,35 +101,45 @@ class Client
         return StreamWrapper::getResource($response->getBody());
     }
 
-    public function listContents($path = '', $recursive = false): array
+    /**
+     * Returns the metadata for a file or folder.
+     * Note: Metadata for the root folder is unsupported.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-get_metadata
+     */
+    public function getMetadata(string $path): array
     {
-        $path = $this->normalizePath($path);
+        $parameters = [
+            'path' => $this->normalizePath($path),
+        ];
 
-        $response = $this->client->post('files/list_folder', [
-            'json' => [
-                'path' => $path,
-                'recursive' => $recursive,
-            ]
-        ]);
-
-        return json_decode($response->getBody(), true);
+        return $this->requestRPC('files/get_metadata', $parameters);
     }
 
+    /**
+     * Get a temporary link to stream content of a file. This link will expire in four hours and afterwards you will get 410 Gone.
+     * Content-Type of the link is determined automatically by the file's mime type.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-get_temporary_link
+     */
     public function getTemporaryLink(string $path): string
     {
-        $path = $this->normalizePath($path);
+        $parameters = [
+            'path' => $this->normalizePath($path),
+        ];
 
-        $response = $this->client->post('files/get_temporary_link', [
-            'json' => [
-                'path' => $path,
-            ],
-        ]);
-
-        $body = json_decode($response->getBody(), true);
+        $body = $this->requestRPC('files/get_temporary_link', $parameters);
 
         return $body['link'];
     }
 
+    /**
+     * Get a thumbnail for an image.
+     * This method currently supports files with the following file extensions: jpg, jpeg, png, tiff, tif, gif and bmp.
+     * Photos that are larger than 20MB in size won't be converted to a thumbnail.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-get_thumbnail
+     */
     public function getThumbnail(string $path, string $format = 'jpeg', string $size = 'w64h64'): string
     {
         $dropboxApiArguments = [
@@ -163,18 +158,52 @@ class Client
     }
 
     /**
-     * @param string $path
-     * @param string $mode
-     * @param string|resource $contents
+     * Starts returning the contents of a folder. If the result's ListFolderResult.has_more field is true, call
+     * list_folder/continue with the returned ListFolderResult.cursor to retrieve more entries.
      *
-     * @return array|false
+     * Note: auth.RateLimitError may be returned if multiple list_folder or list_folder/continue calls with same parameters
+     * are made simultaneously by same API app for same user. If your app implements retry logic, please hold off the retry
+     * until the previous request finishes.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder
      */
-    public function upload(string $path, string $mode, $contents)
+    public function listFolder(string $path = '', bool $recursive = false): array
+    {
+        $parameters = [
+            'path' => $this->normalizePath($path),
+            'recursive' => $recursive,
+        ];
+
+        return $this->requestRPC('files/list_folder', $parameters);
+    }
+
+    /**
+     * Move a file or folder to a different location in the user's Dropbox.
+     * If the source path is a folder all its contents will be moved.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-move
+     */
+    public function move(string $path, string $newPath): array
+    {
+        $parameters = [
+            'from_path' => $this->normalizePath($path),
+            'to_path' => $this->normalizePath($newPath),
+        ];
+
+        return $this->requestRPC('files/move', $parameters);
+    }
+
+    /**
+     * Create a new file with the contents provided in the request.
+     * Do not use this to upload a file larger than 150 MB. Instead, create an upload session with upload_session/start.
+     *
+     * @link https://www.dropbox.com/developers/documentation/http/documentation#files-upload
+     */
+    public function upload(string $path, string $mode, $contents): array
     {
         $dropboxApiArguments = [
             'path' => $this->normalizePath($path),
             'mode' => $mode,
-            'autorename' => true,
         ];
 
         $response = $this->client->post('https://content.dropboxapi.com/2/files/upload', [
@@ -192,7 +221,7 @@ class Client
         return $metadata;
     }
 
-    public function normalizePath(string $path): string
+    protected function normalizePath(string $path): string
     {
         $path = trim($path,'/');
 
@@ -201,5 +230,14 @@ class Client
         }
 
         return '/'.$path;
+    }
+
+    protected function requestRPC(string $endpoint, array $parameters): array
+    {
+        $response = $this->client->post("https://api.dropboxapi.com/2/{$endpoint}", [
+            'json' => $parameters
+        ]);
+
+        return json_decode($response->getBody(), true);
     }
 }
