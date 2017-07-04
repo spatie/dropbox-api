e63b991fbbf1ca7ad7d86a6ad2c90718413f96c2
Wade Urry <wade@iwader.co.uk>
2017-07-04 09:28:44 +0100

    Use machine readable error code on BadRequest exception

--- src/Exceptions/BadRequest.php
+++ src/Exceptions/BadRequest.php
@@ -7,10 +7,36 @@ use Psr\Http\Message\ResponseInterface;
 
 class BadRequest extends Exception
 {
+    /**
+     * The dropbox error code supplied in the response
+     *
+     * @var string
+     */
+    protected $dropboxCode = '';
+
     public function __construct(ResponseInterface $response)
     {
         $body = json_decode($response->getBody(), true);
 
+        if ($response->getStatusCode() === 409) {
+            $this->setDropboxCode($body['error']['.tag']);
+        }
+
         parent::__construct($body['error_summary']);
+    }
+
+    protected function setDropboxCode(string $code)
+    {
+        $this->dropboxCode = $code;
+    }
+
+    /**
+     * Returns the machine readable dropbox error code
+     *
+     * @return string
+     */
+    public function getDropboxCode(): string
+    {
+        return $this->dropboxCode;
     }
 }
