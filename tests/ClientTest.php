0604d51541f15d21678de44d9fb740d3e4d428c0
Mozammil Khodabacchas <hello@moz.im>
2019-02-13 20:15:54 +0400

    Added test for creating a shared link with custom settings.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -620,6 +620,34 @@ class ClientTest extends TestCase
     }
 
     /** @test */
+    public function it_can_create_a_shared_link_with_custom_settings()
+    {
+        $mockGuzzle = $this->mock_guzzle_request(
+            json_encode(['name' => 'math']),
+            'https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer test_token',
+                ],
+                'json' => [
+                    'path' => '/Homework/math',
+                    'settings' => [
+                        'requested_visibility' => 'public',
+                    ],
+                ],
+            ]
+        );
+
+        $client = new Client('test_token', $mockGuzzle);
+
+        $settings = [
+            'requested_visibility' => 'public',
+        ];
+
+        $this->assertEquals(['name' => 'math'], $client->createSharedLinkWithSettings('Homework/math', $settings));
+    }
+
+    /** @test */
     public function it_can_list_shared_links()
     {
         $mockGuzzle = $this->mock_guzzle_request(
