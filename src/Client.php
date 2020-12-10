27d8641b4a00a803ddf33009574017dfcf3282a7
Gerhard Dalenoort <gerhard@duracom.nl>
2020-12-10 10:34:29 +0100

    client remove stray comment

--- src/Client.php
+++ src/Client.php
@@ -702,7 +702,6 @@ class Client
     public function setAccessToken(string $accessToken): self
     {
         $this->tokenProvider = new InMemoryTokenProvider($accessToken);
-        // $this->accessToken = $accessToken;
 
         return $this;
     }
