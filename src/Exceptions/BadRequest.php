738957c6b1db49b149c3efe703f4f20f18b5bea2
Oussama Elgoumri <euvoor@gmail.com>
2022-11-24 18:25:17 +0000

    apply requested changes on pull/108

--- src/Exceptions/BadRequest.php
+++ src/Exceptions/BadRequest.php
@@ -22,18 +22,38 @@ class BadRequest extends Exception
     public function __construct(ResponseInterface $response)
     {
         $this->response = $response;
-
         $content = (string) $response->getBody();
-        $body = json_decode($content, true);
 
-        if (strlen($content) > 0 && is_null($body)) {
-            parent::__construct($content);
-        } else if (! is_null($body)) {
+        $ok = $this->_try_from_json($content);
+
+        if (! $ok) $this->_try_from_plain_text($content);
+    }
+
+    /**
+     * Try reading error from text.
+     */
+    private function _try_from_plain_text($content): bool {
+        if (empty($content)) return false;
+        parent::__construct($content);
+        return true;
+    }
+
+    /**
+     * Try reading error from json.
+     */
+    private function _try_from_json(string $content): bool {
+        $content = json_decode($content, true);
+
+        if (json_last_error() === JSON_ERROR_NONE) {
             if (isset($body['error']['.tag'])) {
-                $this->dropboxCode = $body['error']['.tag'];
+                $this->dropboxCode = $content['error']['.tag'];
             }
 
-            parent::__construct($body['error_summary']);
+            parent::__construct($content['error_summary']);
+
+            return true;
         }
+
+        return false;
     }
 }
